e495e8bbb18aa7f82b954e7cafde8af1c3f7d940
claudsaints <claudiodsantoss7@gmail.com>
2024-09-25 13:42:06 -0300

    ajuste controllers para usuario e adição de novos + jwt teste

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -9,66 +9,108 @@ dotenv.config();
 const saltRounds = parseInt(process.env.SRounds as string);
 const jwtSecret = process.env.JWT_SECRET || 'default-secret';
 
-class User{
-    
+class User {
+
     public async Cadastrar_Novo_Usuario(req: Request, res: Response): Promise<Response> {
-    const { mail, passwd, nome} = req.body;
-    const clUsr = process.env.DB_T_USR;
-    const clEmail = process.env.DB_T_USR_M;
-    const clPwd = process.env.DB_T_USR_P;
-    const clNome = process.env.DB_T_USR_N;
-    
-    try {
-        // Implementação bcrypt
-        const hashedPassword = await bcrypt.hash(passwd, saltRounds);
+        const { mail, passwd, nome } = req.body;
+        const clUsr = process.env.DB_T_USR;
+        const clEmail = process.env.DB_T_USR_M;
+        const clPwd = process.env.DB_T_USR_P;
+        const clNome = process.env.DB_T_USR_N;
 
-        await pool.query(
-            `INSERT INTO ${clUsr} (${clEmail}, ${clPwd}, ${clNome}) VALUES ($1, $2, $3)`,
-            [mail, hashedPassword, nome]
-        );
+        try {
+            // Implementação bcrypt
+            const hashedPassword = await bcrypt.hash(passwd, saltRounds);
 
-        return res.status(201).json({ message: 'Usuário cadastrado com sucesso!' });
-    } catch (error) {
-        console.error('Erro ao cadastrar usuário:', error);
-        return res.status(500).json({ error: 'Erro ao cadastrar usuário' });
+            await pool.query(
+                `INSERT INTO ${clUsr} (${clEmail}, ${clPwd}, ${clNome}) VALUES ($1, $2, $3)`,
+                [mail, hashedPassword, nome]
+            );
+
+            return res.status(201).json({ message: 'Usuário cadastrado com sucesso!' });
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
+            return res.status(500).json({ error: 'Erro ao cadastrar usuário' });
+        }
+
     }
-
-}
 
 
     public async Login(req: Request, res: Response): Promise<Response> {
-    const { mail, passwd } = req.body;
-    const clUsr = process.env.DB_T_USR;
-    const clEmail = process.env.DB_T_USR_M;
-    const clPwd = process.env.DB_T_USR_P as string;
+        const { mail, passwd } = req.body;
+        const clUsr = process.env.DB_T_USR;
+        const clEmail = process.env.DB_T_USR_M;
+        const clPwd = process.env.DB_T_USR_P as string;
 
-    try {
+        try {
 
-        const result = await pool.query(
-            `SELECT ${clPwd} FROM ${clUsr} WHERE ${clEmail} = $1`,
-            [mail]
-        );
+            const result = await pool.query(
+                `SELECT * FROM ${clUsr} WHERE ${clEmail} = $1`,
+                [mail]
+            );
 
-        if (result.rows.length === 0) {
-            return res.status(401).json({ error: 'Credenciais inválidas' });
+            console.log("resultado",result);
+
+            if (result.rows.length === 0) {
+                return res.status(401).json({ error: 'Credenciais inválidas' });
+            }
+
+            const storedHashedPassword = result.rows[0][clPwd];
+            const user_auth = result.rows[0].id
+            const match = await bcrypt.compare(passwd, storedHashedPassword);
+
+            if (match) {
+                // Gerar o token
+                const token = jwt.sign({ email: mail, id: user_auth }, jwtSecret, { expiresIn: '1h' });
+
+                return res.status(200).json({ message: 'Login bem-sucedido', token });
+            } else {
+                return res.status(401).json({ error: 'Credenciais inválidas' });
+            }
+        } catch (error) {
+            console.error('Erro ao fazer login:', error);
+            return res.status(500).json({ error: 'Erro ao fazer login' });
         }
-
-        const storedHashedPassword = result.rows[0][clPwd];
-        const match = await bcrypt.compare(passwd, storedHashedPassword);
-
-        if (match) {
-            // Gerar o token
-            const token = jwt.sign({ email: mail }, jwtSecret, { expiresIn: '1h' });
-
-            return res.status(200).json({ message: 'Login bem-sucedido', token });
-        } else {
-            return res.status(401).json({ error: 'Credenciais inválidas' });
-        }
-    } catch (error) {
-        console.error('Erro ao fazer login:', error);
-        return res.status(500).json({ error: 'Erro ao fazer login' });
     }
-}
+    //incompleto falta adaptaçâo do banco
+    public async determinarPerfil(req: Request, res: Response): Promise<Response> {
+        const { usr_id, altura, peso, genero, objetivos, data } = req.body
+        try {
+            const verificar_dados = await pool.query(`
+                SELECT * FROM User_Dados WHERE User_Default_Id = $1
+            `, [usr_id]);
+
+            if (verificar_dados.rows.length == 0) {
+                const insert = await pool.query(`
+                        INSERT INTO User_Dados VALUES ($1,$2,$3,$4,$5,$6)
+                    `, [usr_id, altura, peso, genero, objetivos, data]);
+                return res.status(200).json({ message: "Dados Salvos Com Sucesso", status: "Inicial" })
+            } else {
+                const update = await pool.query(`
+                    UPDATE User_Dados SET Altura = $1,Peso = $2,Genero= $3,Obj_Peso= $4  WHERE User_Default_Id = $5
+                    `, [altura, peso, genero, objetivos, usr_id])
+                return res.status(200).json({ message: "Dados Salvos Com Sucesso", status: "Subsequente" })
+            }
+        } catch (err) {
+            return res.status(401).json({ err: err })
+        }
+    }
+    //autenticada para dados
+    public async obterDados(req:Request,res:Response): Promise<Response>{
+        const {id} = res.locals
+
+        try{
+            const buscar = await pool.query(`SELECT * FROM User_Dados WHERE User_Default_Id = $1`,[id]);
+            
+            return res.status(200).json({"dados": buscar.rows})
+        }catch(err){
+            return res.status(401).json({err: err});
+        }
+
+
+
+    }
+
 };
 
 export default new User;
--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import user  from '../controllers/UserController';
+import {authenticateToken} from '../middlewares/jwt'
 const routes = Router();
 
 routes.post('/cadastro', user.Cadastrar_Novo_Usuario);
 routes.post('/login', user.Login);
-
+routes.post('/dados', user.determinarPerfil);
+routes.get("/obter",authenticateToken, user.obterDados);
 export default routes;
