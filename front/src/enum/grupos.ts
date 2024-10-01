a8a5b797bdb63ec1c4bd4da19d18e4e1e41514ed
ClauPerretti <claudio.perretti@fatec.sp.gov.br>
2024-10-01 20:51:54 -0300

    criado seletor de categoria na aba de pesquisa

---
+++ front/src/enum/grupos.ts
@@ -0,0 +1,23 @@
+enum grupos {
+            CereaisELeguminosas = 1,
+            HortaliçasTuberosas = 2,
+            FarinhasFéculasEMassas = 3,
+            CocosCastanhasENozes = 4,
+            HortaliçasFolhosasFrutosasEOutras = 5,
+            Frutas = 6,
+            AçúcaresEProdutosDeConfeitaria = 7,
+            SaisECondimentos = 8,
+            CarnesEVísceras = 9,
+            PescadosEFrutosDoMar = 10,
+            EnlatadosEConservas = 11,
+            AvesEOvos = 12,
+            Laticínios = 13,
+            Panificados = 14,
+            CarnesIndustrializadas = 15,
+            BebidasNãoAlcoólicasEInfusões = 16,
+            BebidasAlcoólicas = 17,
+            ÓleosEGorduras = 18,
+            Miscelâneas = 19
+}
+
+export {grupos}
--- front/src/pages/Pesquisa.tsx
+++ front/src/pages/Pesquisa.tsx
@@ -1,23 +1,23 @@
 import { useEffect,useState } from "react"
 import { LinhaSld, StlInput, Nav,FlexDivCo,FlexDiv, StlCaixa,FoodCard} from "../components/index"
 import foods from  '../services/foods'
-import { useNavigate, useParams} from "react-router-dom"
-
+import { grupos } from "../enum/grupos"
    
 export function Pesquisa(){
-   const [alimentos,setAlimento] = useState([])
-        const {querry} = useParams();
+   const [tipo, setTipo] = useState<number >(1);
+   const [alimentos,setAlimento] = useState([]);
+   const [triger,setTriger] = useState(false);
     useEffect(() =>{
 
-        foods.buscarCategoria(1,setAlimento);
-        if(querry){
-            foods.pesquisar(querry, setAlimento)
+        foods.buscarCategoria(tipo,setAlimento);
+        if(triger){
+            foods.pesquisar(Querry, setAlimento)
         }
-    },[])
+    },[tipo, triger])
 
-    const navigate = useNavigate();
-
+    
     const [Querry,setQuerry] = useState("");
+    
 
     return(
         <>
@@ -31,8 +31,33 @@ export function Pesquisa(){
             </Nav>
                 <FlexDiv>
                     <StlInput onChange={e => setQuerry(e.target.value)} bcolor='#ffffff' width='60%' height='40px' placeholder='Pesquisa Alimento' />
-                    <button onClick={() => {navigate(`/pesquisa/${Querry}`), window.location.reload()}}>Pesquisa</button>
+                    <button onClick={() => {setTriger(true)}}>Pesquisa</button>
                 </FlexDiv>
+                <FlexDiv>
+                <label>Categoria:</label>
+                <select value={tipo} onChange={(e) => {setTipo(e.target.value); setTriger(false)}}>
+                    <option value={grupos.CereaisELeguminosas}>Cereais e leguminosas</option>
+                    <option value={grupos.HortaliçasTuberosas}>Hortaliças tuberosas</option>
+                    <option value={grupos.FarinhasFéculasEMassas}>Farinhas féculas e massas</option>
+                    <option value={grupos.CocosCastanhasENozes}>Cocos castanhas e nozes</option>
+                    <option value={grupos.HortaliçasFolhosasFrutosasEOutras}>Hortaliças folhosas frutosas e outras</option>
+                    <option value={grupos.Frutas}>Frutas</option>
+                    <option value={grupos.AçúcaresEProdutosDeConfeitaria}>Açúcares e produtos de confeitaria</option>
+                    <option value={grupos.SaisECondimentos}>Sais e condimentos</option>
+                    <option value={grupos.CarnesEVísceras}>Carnes e vísceras</option>
+                    <option value={grupos.PescadosEFrutosDoMar}>Pescados e frutosDoMar</option>
+                    <option value={grupos.EnlatadosEConservas}>Enlatados e conservas</option>
+                    <option value={grupos.AvesEOvos}>Aves e ovos</option>
+                    <option value={grupos.Laticínios}>Laticínios</option>
+                    <option value={grupos.Panificados}>Panificados</option>
+                    <option value={grupos.CarnesIndustrializadas}>Carnes industrializadas</option>
+                    <option value={grupos.BebidasNãoAlcoólicasEInfusões}>Bebidas não alcoólicas e infusões</option>
+                    <option value={grupos.BebidasAlcoólicas}>Bebidas alcoólicas</option>
+                    <option value={grupos.ÓleosEGorduras}>Óleos e gorduras</option>
+                    <option value={grupos.Miscelâneas}>Miscelâneas</option>        
+                </select>
+            </FlexDiv>
+
                 
             </FlexDivCo>
             {
