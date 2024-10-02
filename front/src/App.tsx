8dcbd8de0d3558626723b3f9bd9d5b82cde1b5be
brn-lc <brenol_boff@hotmail.com>
2024-10-02 19:15:21 -0300

    Pagina de cadastro de alimentos

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,27 @@
-import { Welcome, Login,Cadastro, PerfilConfig,Pesquisa,Perfil} from './pages/index';
-import {BrowserRouter,  Route,  Routes} from 'react-router-dom'
-
+import {
+  Welcome,
+  Login,
+  Cadastro,
+  PerfilConfig,
+  Pesquisa,
+  Perfil,
+  CadastroAlimento,
+} from "./pages/index";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Welcome/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/cadastro' element={<Cadastro/>}/>
-        <Route path='/perfil' element={<Perfil/>}/>
-        <Route path='/perfil/config' element={<PerfilConfig/>}/>
-        <Route path='/pesquisa' element={<Pesquisa/>}/>
-        <Route path='/pesquisa/:querry' element={<Pesquisa/>}/>
-      </Routes>    
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/perfil/config" element={<PerfilConfig />} />
+        <Route path="/pesquisa" element={<Pesquisa />} />
+        <Route path="/pesquisa/:querry" element={<Pesquisa />} />
+        <Route path="/cadastroalimento" element={<CadastroAlimento />} />
+      </Routes>
     </BrowserRouter>
   );
-  
 }
