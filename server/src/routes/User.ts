c2174d08f95b10e38d0cff6ba90b72a9af9abf7d
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-10-14 21:13:57 -0300

    adicionando rota de obter produto por data no /user

--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import user  from '../controllers/UserController';
-import {authenticateToken} from '../middlewares/jwt'
+import user from '../controllers/UserController';
+import { authenticateToken } from '../middlewares/jwt'
 const routes = Router();
 
 routes.post('/cadastro', user.Cadastrar_Novo_Usuario);
 routes.post('/login', user.Login);
-routes.post('/dados',authenticateToken, user.determinarPerfil);
-routes.get("/obter",authenticateToken, user.obterDados);
-routes.get("/obter/consumo",authenticateToken,user.obterConsumo);
+routes.post('/dados', authenticateToken, user.determinarPerfil);
+routes.get("/obter", authenticateToken, user.obterDados);
+routes.get("/obter/consumo", authenticateToken, user.obterConsumo);
+routes.get("/obter/consumo/:data", authenticateToken, user.obterConsumoData);
 
 export default routes;
