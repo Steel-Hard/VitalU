2013847701ed5aaf89b4345dc9af9d591461c0e1
Claudio Dos Santos <161241590+claudsaints@users.noreply.github.com>
2024-11-07 21:24:26 -0300

    Update obterTempo.ts

--- front/src/utils/obterTempo.ts
+++ front/src/utils/obterTempo.ts
@@ -1,16 +1,10 @@
 export function TimeStamp(): string {
     const date = new Date();
-    
-    // Obter as partes da data no horário UTC
-    const year = date.getUTCFullYear();
-    const month = ('00' + (date.getUTCMonth() + 1)).slice(-2); // Mês começa do 0, então somamos 1
-    const day = ('00' + date.getUTCDate()).slice(-2);
-    const hours = ('00' + date.getUTCHours()).slice(-2);
-    const minutes = ('00' + date.getUTCMinutes()).slice(-2);
-    const seconds = ('00' + date.getUTCSeconds()).slice(-2);
-
-    // Formatar o timestamp no formato UTC (YYYY-MM-DD HH:MM:SS)
-    const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    
+    const timestamp = date.getFullYear() + '-' +
+        ('00' + (date.getMonth() + 1)).slice(-2) + '-' +
+        ('00' + date.getDate()).slice(-2) + ' ' + 
+        ('00' + date.getHours()).slice(-2) + ':' + 
+        ('00' + date.getMinutes()).slice(-2) + ':' + 
+        ('00' + date.getSeconds()).slice(-2);
     return timestamp;
 }
